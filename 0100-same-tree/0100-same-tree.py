b4534ff3d9cfb2997bcc758e4b71ff3421981be1
ashimaryal25-ops <ashimaryal25@gmail.com>
2026-06-12 01:25:07 -0400

    Time: 0 ms (100%), Space: 12.5 MB (23.52%) - LeetHub

---
+++ 0100-same-tree/0100-same-tree.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def isSameTree(self, p, q):
+
+        if p == None and q == None:
+            return True
+                
+        if p == None or q == None:
+            return False
+        
+        if p.val != q.val:
+            return False
+                
+        # The Leap of Faith
+        return self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right)
