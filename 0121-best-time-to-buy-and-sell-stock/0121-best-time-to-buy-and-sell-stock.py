5be622c3b5083de8b9b5e3d88980a5f87a84518d
Ashim Aryal <ashimaryal25@gmail.com>
2026-06-23 21:25:04 -0400

    Time: 131 ms (29.38%), Space: 18.9 MB (74.9%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
@@ -1,16 +1,26 @@
 class Solution(object):
     def maxProfit(self, prices):
-        minSeen = float('inf')
+        #using pointers
+
+        left = 0
+
+        right = 1
 
         maxProfit = 0
-
-        for price in prices:
-
-
-            curProfit = price - minSeen
-
-           
-            minSeen = min( minSeen, price)
-            maxProfit = max( curProfit, maxProfit)
         
-        return maxProfit
+        while right < len( prices ): 
+
+            curProfit = prices[right] - prices[left]
+
+            if prices[left] > prices[right]:
+                left = right
+            
+            maxProfit = max( curProfit, maxProfit)
+
+            right += 1
+
+
+        return maxProfit    
+
+
+
