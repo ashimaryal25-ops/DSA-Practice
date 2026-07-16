6e749ce4d2e9ca60ac430728ab8ec811e6f8e58e
Ashim Aryal <ashimaryal25@gmail.com>
2026-07-16 00:26:38 -0400

    Time: 566 ms (41.2%), Space: 33.8 MB (29.08%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.py
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.py
@@ -2,10 +2,10 @@ class Solution(object):
     def longestCommonSubsequence(self, text1, text2):
         m = len(text1)
         n = len(text2)
-        
+
         dp = [[0] * (n + 1) for _ in range(m + 1)]
         
-        # 2. Sweep the grid starting at index 1
+        # Sweep the grid starting at index 1
         for r in range(1, m + 1):
             for c in range(1, n + 1):
                 
